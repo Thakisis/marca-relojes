15f941a599fd245550be60e593c99914fcdbdcdf
Thakisis <sub722@gmail.com>
2025-03-17 22:49:00 +0100

    edit watch

--- components/canvas/canvas.jsx
+++ components/canvas/canvas.jsx
@@ -2,14 +2,14 @@ import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { Environment } from '@react-three/drei';
-import { Daytona } from './Daytona2';
+import { Daytona } from './Daytona';
 function Canvas3d({ }) {
     return (
         <Canvas >
             <ambientLight intensity={0.5} />
             <pointLight position={[10, 10, 10]} />
             <Suspense fallback={null}>
-                <Daytona />
+                <Daytona scale={0.2} position={[3, 0, 0]} rotation={[Math.PI / 2, 0, Math.PI / 10]} />
                 <Environment files='/grey1.exr' environmentRotation={[0, Math.PI, Math.PI / 2]}
                     environmentIntensity={5}
                 />
--- components/canvas/materials.js
+++ components/canvas/materials.js
@@ -1,10 +1,25 @@
 import * as THREE from "three";
-export function createMaterials() {
-	const rolexGold = new THREE.MeshStandardMaterial({
+export function createMaterials(materials) {
+	const rolexGolda = new THREE.MeshStandardMaterial({
 		color: new THREE.Color(0xe0875e), // Warmer rose gold color to match Rolex Everose gold
 		metalness: 1,
 		roughness: 0.01,
 		envMapIntensity: 1.2,
+	});
+	const rolexGold = new THREE.MeshPhysicalMaterial({
+		color: new THREE.Color(0.944, 0.776, 0.373), // Warmer rose gold color to match Rolex Everose gold
+		metalness: 1,
+		roughness: 0.0,
+		envMapIntensity: 1.2,
+		specularColor: new THREE.Color(0.981, 0.979, 0.781),
+	});
+	const rolexGold2 = new THREE.MeshPhysicalMaterial({
+		color: new THREE.Color(0xe0875e),
+		iridescenceIOR: 1.5,
+		iridescenceThicknessRange: [100, 1000],
+		metalness: 1,
+		roughness: 0.3,
+		envMapIntensity: 1.0,
 	});
 
 	// Create steel material for contrast (like the Rolesor combination)
@@ -16,8 +31,9 @@ export function createMaterials() {
 		roughness: 0.3,
 		envMapIntensity: 1.0,
 	});
+
 	const rolexFind = new THREE.MeshBasicMaterial({
 		color: new THREE.Color(0x00ff00),
 	});
-	return { rolexGold, rolexSteel, rolexFind };
+	return materials;
 }
